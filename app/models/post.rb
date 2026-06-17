0f637f4985087928560a9644763b00ad551f40b3
Yaswanth Panatala <yashpanatala@gmail.com>
2026-06-17 16:27:02 +0530

    Added validations in the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10000
+
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
+
+  validates_inclusion_of :is_bloggable, in: [true, false]
 end
