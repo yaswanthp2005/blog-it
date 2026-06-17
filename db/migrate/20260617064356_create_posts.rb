242a20ff20e476c072ddf3e960030793510e49f8
Yaswanth Panatala <yashpanatala@gmail.com>
2026-06-17 13:01:42 +0530

    Added Post model

---
+++ db/migrate/20260617064356_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :upvotes, default: 0, null: false
+      t.integer :downvotes, default: 0, null: false
+      t.boolean :is_bloggabel, default: false
+      
+      t.timestamps
+    end
+  end
+end
